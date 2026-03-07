3b750b70f1c321bf6ad276ec0a3bcc7c9c067f2e
Michael Azcueta <azcuetajjmt@gmail.com>
2026-03-07 21:21:08 +0800

    add .env configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_FORMSPREE_ENDPOINT: envField.string({ context: 'client', access: 'public' }),
+      PUBLIC_TURNSTILE_SITE_KEY: envField.string({ context: 'client', access: 'public' }),
+    },
+  },
+})
