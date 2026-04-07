46f767e56334ca7730478f6af334b25fde03141a
Michael Azcueta <azcuetajjmt@gmail.com>
2026-04-07 16:01:43 +0800

    playwright config and workflow updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run build && npm run preview',
-    url: 'http://localhost:4321',
+    url: 'http://localhost:4322',
     reuseExistingServer: !process.env.CI,
   },
 })
