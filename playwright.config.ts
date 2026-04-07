d83bc1eaa116bf6d1c42a2ff34c30e4b0d54f73f
Michael Azcueta <azcuetajjmt@gmail.com>
2026-04-07 14:55:48 +0800

    fix failing playwright workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
+    command: 'npm run build && npm run preview',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
   },
